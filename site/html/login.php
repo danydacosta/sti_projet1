c7385e259edf5375d4eee782776ecbf7f08364fd
Stefan Simeunovic <labud1998@gmail.com>
2021-10-06 00:55:16 +0200

    Login first steps

--- site/html/login.php
+++ site/html/login.php
@@ -1,1 +1,54 @@
-// permet de se loguer, redirige sur index
+<?php
+
+
+    if (isset($_POST['inputLogin'])) {
+        try {
+            include 'dbConnect.php';
+
+            $login = $_POST['inputLogin'];
+
+            $result = $file_db->query("SELECT * FROM user WHERE login = '{$login}'");
+            print_r($result->fetch());
+            //print_r($result);
+        } catch(PDOException $e) {
+            // Print PDOException message
+            echo $e->getMessage();
+        }
+
+    }
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login page</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+  <form class="form-signin" method="POST">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputLogin" class="sr-only">Login</label>
+      <input type="text" id="inputLogin" name="inputLogin" class="form-control" placeholder="Login" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Password" required>
+      <input type="submit" class="btn btn-lg btn-primary btn-block" value="Submit">
+  </form>
+
+
+  </body>
+</html>
