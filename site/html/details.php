88b1b81cf659224653eb6db40e70ccbc78cc0c0d
danydacosta <danydc@powerfoxsa.ch>
2021-10-10 20:45:47 +0200

    Update details.php

--- site/html/details.php
+++ site/html/details.php
@@ -31,7 +31,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Détails</title>
+    <title>Détails de <?php echo $message['sujet']; ?></title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
