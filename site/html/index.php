ec02eae5329a9d0bfa9be9720c0d9d4da3b8b03b
danydacosta <danydc@powerfoxsa.ch>
2021-10-10 21:34:17 +0200

    disconnect

--- site/html/index.php
+++ site/html/index.php
@@ -4,9 +4,29 @@
         header('Location: login.php');
     }
 
+    if(isset($_GET['disconnect'])) {
+        unset($_SESSION['userLogin']);
+        header('Location: login.php');
+    }
+
     include('dbConnect.php');
 
     $messages =  $file_db->query('SELECT * FROM message WHERE destinataire = "'.$_SESSION['userLogin'].'"')->fetchAll();
+
+    if(isset($_GET['del'])) {
+        // le message à supprimer doit être un message de l'utilisateur
+        $found = false;
+        foreach($messages as $key => $value) {
+            if(array_search($_GET['del'], $value) !== false) {
+                $found = true;
+                unset($messages[$key]);
+            }
+        } 
+
+        if($found) {
+            $file_db->exec("DELETE FROM message WHERE id = ".$_GET['del']);
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,9 +47,25 @@
     <div class="wrapper container">
         <div class="row">
             <div class="col">
-                <?php echo $_SESSION['userLogin']; ?>
             </div>
             <div class="col col-md-6">
+                <br>
+                <div class="float-end">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                    </svg>
+                    <b>
+                        <?php echo $_SESSION['userLogin']; ?>
+                    </b>
+
+                    <button type="button" class="btn btn-danger btn-sm" style="margin-left: 10px;" onclick="window.location.href = '/index.php?disconnect=true'">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+                            <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+                            <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z"/>
+                        </svg>
+                    </button>
+                </div>
+
                 <br />
                 <h1>Messages</h1>
                 <br />
@@ -64,7 +100,7 @@
                                                     <path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z" />
                                                 </svg>
                                             </button>
-                                            <button type="button" class="btn btn-danger btn-sm">
+                                            <button type="button" class="btn btn-danger btn-sm" onclick="if (confirm(\'Supprimer le message ?\')){ window.location.href = \'/index.php?del='.$row['id'].'\'; }else{event.stopPropagation(); event.preventDefault();};">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                                     <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
--- site/html/new_msg.php
+++ site/html/new_msg.php
@@ -3,6 +3,11 @@
     $error = false;
     $success = false;
 
+    if(isset($_GET['disconnect'])) {
+        unset($_SESSION['userLogin']);
+        header('Location: login.php');
+    }
+
     if(!isset($_SESSION['userLogin'])) {
         header('Location: login.php');
     } if(isset($_POST['submit'])) {
@@ -42,6 +47,23 @@
 
             </div>
             <div class="col col-md-6">
+                <br>
+                <div class="float-end">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                    </svg>
+                    <b>
+                        <?php echo $_SESSION['userLogin']; ?>
+                    </b>
+
+                    <button type="button" class="btn btn-danger btn-sm" style="margin-left: 10px;" onclick="window.location.href = '/index.php?disconnect=true'">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+                            <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+                            <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117zM11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5zM4 1.934V15h6V1.077l-6 .857z"/>
+                        </svg>
+                    </button>
+                </div>
+                <br />
                 <?php
                     if($error) {
                        echo '<div class="alert alert-danger" role="alert">
